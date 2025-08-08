9160df9963000826c66116f1818fc4b6b5ffbe9a
Rony Duarte Gomez <ronnyduartegomez@gmail.com>
2025-08-08 00:59:06 -0500

    Avance1

--- app.js
+++ app.js
@@ -2,21 +2,55 @@
 
 let listaNombreAmigos = [];
 
+function validaciones(nombre){
+
+     if(nombre==''){
+          alert('!Debe ingresar texto!');
+          return 0;
+     }else{
+          if (listaNombreAmigos.includes(nombre)) {
+               alert('El nombre de amigo ya fue agregado');
+               return 0;
+          }
+          else{
+               return 1;
+          }
+     }
+
+}
+
 function agregarAmigo(){
 
     
-    let NombreAmigos = document.getElementById('amigo').value;
+    let nombreAmigos = document.getElementById('amigo').value.toUpperCase();;
 
-    if (listaNombreAmigos.includes(NombreAmigos)) {
-         alert('El nombre de amigo ya fue agregado');
-    } else {
-         listaNombreAmigos.push(NombreAmigos);
-         console.log(NombreAmigos);
+     let valida = validaciones(nombreAmigos);
+
+     if (valida ==1){
+         listaNombreAmigos.push(nombreAmigos);
+         console.log(nombreAmigos);
          console.log(listaNombreAmigos);
-    }
 
-
-
+         let elementoHTML = document.querySelector("#listaAmigos");
+         elementoHTML.innerHTML = listaNombreAmigos;
     
+         limpiar()
 
+    }
+ 
+
+}
+
+function sortearAmigo(){
+
+     let numeroGenerado =  Math.floor(Math.random()*listaNombreAmigos.length);
+
+     let elementoHTML1 = document.querySelector("#resultado");
+    elementoHTML1.innerHTML = listaNombreAmigos[numeroGenerado];
+}
+
+function limpiar(){
+
+     document.querySelector('#amigo').value = '';
+         document.getElementById('amigo').focus();
 }
